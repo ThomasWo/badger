407d1816c0fc2ddb950b9c759d17a9e8ec9ca267
Laura Mosher <laura.trevisan@gmail.com>
2015-05-04 20:22:41 -0400

    Added Event and Attendee tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426202817) do
+ActiveRecord::Schema.define(version: 20150505001951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "company"
+    t.string   "twitter_handle"
+    t.string   "shirt_size"
+    t.text     "dietary_restrictions"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
